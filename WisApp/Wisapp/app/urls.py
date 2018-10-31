7b1d76eb076ce3c92bd3313f939c4e7bae2b1ed8
Ali Villegas <alibryan.villegas@gmail.com>
2018-10-30 18:21:30 -0600

    added interfaces
    - Profile
    - Story
    - Events
    - categories
    
    Template interfaces have been addes
    
    -Secret key removes

--- WisApp/Wisapp/app/urls.py
+++ WisApp/Wisapp/app/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('login/', views.login, name='userLogin'),
-    path('home/', views.home, name='home'),
-
+    path('inicio/', views.home, name='home'),
+    path('categorias/', views.categories, name='categories'),
+    path('eventos/', views.events, name='events'),
+    path('perfil/', views.profileCreator, name='profile'),
+    path('perfil-personal/', views.profileNonCreator, name='profile'),
+    path('historia/', views.story, name='story'),
 ]
--- WisApp/Wisapp/app/views.py
+++ WisApp/Wisapp/app/views.py
@@ -13,3 +13,22 @@ def home(request):
     context = {}
     return render(request, 'app/home.html', context)
 
+def categories(request):
+    context = {}
+    return render(request, 'app/categories.html', context)
+
+def events(request):
+    context = {}
+    return render(request, 'app/eventos.html', context)
+
+def profileCreator(request):
+    context = {}
+    return render(request, 'app/profileCreator.html', context)
+
+def profileNonCreator(request):
+    context = {}
+    return render(request, 'app/profileNonCreator.html', context)
+
+def story(request):
+    context = {}
+    return render(request, 'app/story.html', context)
